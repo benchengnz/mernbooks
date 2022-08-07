36b67eda8d0dc2c642d66cdb6feb1fd2b02635fc
ben cheng <nz_fox@hotmail.com>
2022-08-07 15:36:02 +1200

    added client project

--- app.js
+++ app.js
@@ -3,10 +3,11 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
-
+const port = process.env.PORT || 5000;
+const routes = require('./routes/api/books');
 connectDB();
 app.get('/', (req, res) => res.send('Hello world! Now you see me @@ 2.'));
 
-const port = process.env.PORT || 8082;
+app.use('/api', routes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
